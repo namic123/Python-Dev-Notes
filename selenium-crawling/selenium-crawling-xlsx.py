4096f0a4d6d4a88d2bfd3a69f5f375704d16d705
jspark <namic123@naver.com>
2025-06-14 01:21:13 +0900

    엑셀 파일 생성 및 반복문으로 데이터를 쓰고 저장 예제

--- selenium-crawling/selenium-crawling-xlsx.py
+++ selenium-crawling/selenium-crawling-xlsx.py
@@ -5,9 +5,13 @@ xlsxFile = openpyxl.Workbook()
 
 # 생성한 파일에서 시트 생성
 xlsxSheet = xlsxFile.active
+#
+# # 시트 특정 셀에 데이터 입력
+# xlsxSheet.cell(row=1, column=1).value = "hi"
 
-# 시트 특정 셀에 데이터 입력
-xlsxSheet.cell(row=1, column=1).value = "hi"
+# 반복문으로 데이터 입력
+for i in range(10):
+    xlsxSheet.cell(row= 1 , column= i + 1).value = i +1
 
 # 저장
 xlsxFile.save('result.xlsx')
